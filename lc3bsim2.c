ffaeacc0d77f4573acaa2299a3888223511d6132
enriquecallado <kikecallado@gmail.com>
2018-02-07 19:45:47 -0600

    stuff

--- lc3bsim2.c
+++ lc3bsim2.c
@@ -438,6 +438,7 @@ void process_instruction() {
                 NEXT_LATCHES = CURRENT_LATCHES;
                 /*imm5*/
                 if (lowByte & 0x20 == 1) {
+                        /*we need to interpret negatives. lowbyte & 0x1F is always read as a positive*/
 			NEXT_LATCHES.REGS[DR] = CC_SETTER = SR1 + (lowByte & 0x1F);
 		}
 		/*2 SR*/
@@ -447,39 +448,40 @@ void process_instruction() {
                 }
                 setCCs(CC_SETTER);
                 NEXT_LATCHES.PC = NEXT_LATCHES.PC + 2;
-                
-                /*you don't change condition codes and you copy each register one by one*/
-                int i = 0;
-		for (i = 0; i < LC_3b_REGS; i++)
-		{
-			if (DR == i) {
-				/*imm5*/
-				if (lowByte & 0x20 == 1) {
-					NEXT_LATCHES.REGS[DR] = SR1 + (lowByte & 0x1F);
-				}
-				/*2 SR*/
-				else {
-					NEXT_LATCHES.REGS[DR] = SR1 + (lowByte & 0x7);
-				}
-			}
-			else {
-				NEXT_LATCHES.REGS[i] = CURRENT_LATCHES.REGS[i];
-			}
-		}
-		NEXT_LATCHES.PC = CURRENT_LATCHES.PC + 2;
-		NEXT_LATCHES.N = CURRENT_LATCHES.N;
-		NEXT_LATCHES.Z = CURRENT_LATCHES.Z;
-		NEXT_LATCHES.P = CURRENT_LATCHES.P;
+                           
 	}
 	/*AND*/
 	if (highByte >> 4 == 5)
-	{
+	{  
+                DR = (highByte >> 1) & 0x7;
+		SR1 = (highByte & 0x1 << 2) + ((lowByte >> 6) & 0x3);
+                
+                /*all the latches that we don't change will be the same*/
+                NEXT_LATCHES = CURRENT_LATCHES;
+                /*imm5*/
+                if (lowByte & 0x20 == 1) {
+                        /*sign extend (lowByte & 0x1F)*/
+			NEXT_LATCHES.REGS[DR] = CC_SETTER = SR1 & (lowByte & 0x1F);
+		}
+		/*2 SR*/
+                else {
+                        /**/
+                        SR2 = CURRENT_LATCHES.REGS[lowByte & 0x7];
+                        NEXT_LATCHES.REGS[DR] = CC_SETTER = SR1 & SR2;
+                }
+                setCCs(CC_SETTER);
+                NEXT_LATCHES.PC = NEXT_LATCHES.PC + 2;
+            
 
 	}
 	/*BR*/
 	if (highByte >> 4 == 0)
 	{
-
+            NEXT_LATCHES = CURRENT_LATCHES;
+            NEXT_LATCHES.PC = NEXT_LATCHES.PC + 2;
+            if((CURRENT_LATCHES.N == highByte >> 3 & 0x1) ||(CURRENT_LATCHES.N == highByte >> 2 & 0x1) || (CURRENT_LATCHES.N == highByte >> 1 & 0x1) ){
+                 
+            }
 	}
 	/*JMP*/
 	if (highByte >> 4 == 12)
