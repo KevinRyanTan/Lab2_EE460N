df639b0a29f7be13cde4f0db47e159a60f02d6ca
enriquecallado <kikecallado@gmail.com>
2018-02-10 21:37:54 -0600

    memory stuff

--- text1.asm
+++ text1.asm
@@ -1,21 +1,17 @@
         .ORIG x3000             ; 	
 	LEA R0, TEN             ;This instruction will be loaded into memory location x3000
-	LDW R1, R0, #0          ;
-        AND R1, R1, x8          ;R1<-8
-        ADD R1,R1, #-16         ;R1<-(-8)
-        AND R2, R2, #0          ;
-        ADD R2, R1, #0         
-        RSHFL R2, R2, #1        ;
-START	ADD R1, R1, R1          ;
-        ADD R2, R2, #-1         ;
-	BRZ DONE                ;
-	BR START                ;
-				;blank line
-
-DONE	AND R3, R3, #0
-		STB R1, R3, #31 
+	LDW R1, R0, #0          ;R1<-10
+        LEA R0, ADDR            ;
+        LDW R2, R0, #0          ;R2<-0x4000
+        STB R1, R2, #1          ;
+        LEA R0,SMALL            ;
+        LDW R1, R0, #-1         ;R1<- x4356
+        STW R1, R0, #0          ; 
 	TRAP x25;		;The last executable instruction
-TEN	.FILL x0000A;		;This is 10 in 2's comp, hexadecimal
+TEN	.FILL x0000A		;This is 10 in 2's comp, hexadecimal
+ADDR    .FILL x4000            ;
+BIG     .FILL x4356            ;
+SMALL   .FILL x0001            ;
 	.END			;The pseudo-op, delimiting the source program
         
         
